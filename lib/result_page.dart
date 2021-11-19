4739017bdd55cf8e45cc1ce3ac859fdd2257f48e
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2021-11-19 19:21:52 +0700

    Added share button on result page.

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tomodachi/main.dart';
+import 'package:share_plus/share_plus.dart';
 
 // ignore: must_be_immutable
 class ResultPage extends StatefulWidget {
@@ -14,38 +15,55 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  void share(BuildContext context, String question, String answer) {
+    RenderBox? box = context.findRenderObject() as RenderBox;
+
+    String msg = 'Q: $question?\n A: $answer';
+    Share.share(msg,
+        subject: 'และนี่ก็คือ! คำตอบที่พวกนายต้องการยังไงล่ะ!!!',
+        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Result')),
-        body: Center(
-            child: Padding(
+      appBar: AppBar(title: const Text('Result')),
+      body: Center(
+        child: Padding(
           padding: const EdgeInsets.all(15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
                 'Q: ${widget.question}?',
-                style: TextStyle(fontSize: 24),
+                style: const TextStyle(fontSize: 24),
               ),
               const SizedBox(height: 5),
               Text(
                 'A: ${widget.answer}',
-                style: TextStyle(fontSize: 24),
+                style: const TextStyle(fontSize: 24),
               ),
               const SizedBox(height: 30),
               TextButton(
                   onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => MyApp()));
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const MyApp()));
                   },
-                  child: Text('Go back', style: TextStyle(fontSize: 22)),
+                  child: const Text('Go back', style: TextStyle(fontSize: 22)),
                   style: TextButton.styleFrom(
                       fixedSize: const Size(150, 50),
                       primary: Colors.white,
                       backgroundColor: Colors.green.shade300)),
             ],
           ),
-        )));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          share(context, widget.question, widget.answer);
+        },
+        child: const Icon(Icons.share_rounded, size: 35),
+      ),
+    );
   }
 }
